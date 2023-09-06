6fd215601c082b05cdbb6f6bd551adccc066cb9a
EXISTINGG <1906054192@qq.com>
2023-09-06 13:42:55 +0800

    可关闭提示

--- src/utils/message.js
+++ src/utils/message.js
@@ -2,16 +2,22 @@ import { ElMessage } from 'element-plus'
 
 export const success = (msg) => {
   ElMessage({
+    showClose: true,
     message: msg || 'Congrats, this is a success message.',
     type: 'success',
   })
 }
 export const warning = (msg) => {
   ElMessage({
+    showClose: true,
     message: msg || 'Warning, this is a warning message.',
     type: 'warning',
   })
 }
 export const error = (msg) => {
-  ElMessage.error(msg || 'Oops, this is a error message.')
+  ElMessage({
+    showClose: true,
+    message: msg || 'Oops, this is a error message.',
+    type: 'error',
+  })
 }
